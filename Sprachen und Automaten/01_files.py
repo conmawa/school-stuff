0c3d0975036cdbd2c7140a08e3ef80da6d1d2d02
Constantin <157481612+conmawa@users.noreply.github.com>
2024-09-13 12:29:56 +0200

    Add files via upload

---
+++ Sprachen und Automaten/01_files.py
@@ -0,0 +1,13 @@
+## files ##
+
+with open("H:\\mein_satz.txt", "w") as file:
+    ausgabe = file.write("Hallo")
+
+with open("H:\\mein_satz.txt", "a") as file:
+    ausgabe = file.write("\nWie gehts, wie stehts")
+
+
+with open("H:\\mein_satz.txt", "r") as file:
+    ausgabe = file.read()
+
+print(ausgabe)
