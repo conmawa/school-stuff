4909f73e074168992af5f900e1c72d36fd64e322
Constantin <157481612+conmawa@users.noreply.github.com>
2024-12-19 10:28:46 +0100

    add safe text option to KI-project

--- Künstliche Intelligenz/ki-projekt.py
+++ Künstliche Intelligenz/ki-projekt.py
@@ -24,11 +24,16 @@ def main(image_path):
     return text
     
 def import_image():
+    global extracted_text
     file_path = filedialog.askopenfilename()
     if file_path:
         extracted_text = main(file_path)
         text_widget.delete(1.0, "end")
         text_widget.insert("end", extracted_text)
+
+def safe_text():
+    with open('text.txt', mode = 'w') as text_file:
+        text_file.write(extracted_text)
 
 window = ctk.CTk()
 window.title('Texterkennung aus Bildern')
@@ -36,12 +41,15 @@ window.resizable(1,1)
 
 text_widget = ctk.CTkTextbox(window, width=700, height=400, font=("Arial", 14))
 title_label = ctk.CTkLabel(window, text="Texterkennung aus Bildern",font=("Arial", 20))
-upload_button = ctk.CTkButton(window, text = 'Read image', command = import_image)
+upload_button = ctk.CTkButton(window, text = 'Read image', command = import_image, font=("Arial", 20))
+safe_button = ctk.CTkButton(window, text = 'Safe Text', command = safe_text, font=("Arial", 20))
 
 window.config() 
 title_label.grid(column=1, row=0, padx=5, pady=5, columnspan=2)
 upload_button.grid(column = 1, row = 1, pady = 5, padx = 5)
+safe_button.grdi(column = 2, row = 1, pady = 5, padx = 5) 
 text_widget.grid(column=1, row=2, pady=10, padx=5)
+
 window.grid_rowconfigure(2, weight=1)
 window.grid_columnconfigure(1, weight=1)
 
