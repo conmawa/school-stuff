f8e498a6aebde50c26e3a12b66570e7d1f160e73
conmawa <constantin.liepelt@online.de>
2025-02-04 09:57:20 +0100

    abi

---
+++ Abivorbereitung/binaer.py
@@ -0,0 +1,30 @@
+def einlesen():
+    with open('Abivorbereitung\zahlen.csv', "r") as file: # CSV-Datei einlesen
+        zahlen = file.read().split("\n") # Zeilenweise trennen
+        return zahlen
+    
+def even(binaer):
+    if binaer[0] == '0': # erste Stelle gleich 0?
+        integer = int(binaer, 2) # wenn ja, dann umwandeln
+    else:
+        integer = int(binaer, 2) - 256 # wenn nein, dann umwandeln und minus 256 rechnen
+    
+    if integer % 2 == 0: # gerade?
+        return True
+
+def positiv(binaer):
+    zahl = list(binaer) # in Liste umwandeln
+    if zahl[0] == '0': # erste Stelle gleich 0?
+        return True # wenn ja, dann positiv
+    
+def add_to_file(zahl):
+    with open('Abivorbereitung\menge_c.csv', "w") as file:  # CSV-Datei schreiben
+        file.write(zahl + "\n") # Zahl schreiben
+    
+def check():
+    zahlen = einlesen() # Zahlen einlesen
+    for zahl in zahlen: # für jede Zahl
+        if even(zahl) and positiv(zahl): # wenn gerade und positiv
+            add_to_file(zahl) # dann in Datei schreiben
+          
+check()
